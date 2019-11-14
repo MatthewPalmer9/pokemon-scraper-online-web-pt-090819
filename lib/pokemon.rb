5c57228322378b4d36ae05c3c1c3f8bfcbee56a3
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-14 20:03:21 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,6 +19,6 @@ class Pokemon
     name = pokemon[0]
     type = pokemon[1]
     binding.pry
-    new_pokemon = self.new(id: id, name: name, type: type, db: db)
+    new_pokemon = self.new(name: name, type: type, db: db)
   end
 end
