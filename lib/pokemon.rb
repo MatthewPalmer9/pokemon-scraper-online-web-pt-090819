0074017fd22f0613f912820650533d3c7c20a859
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-14 19:44:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,7 +8,7 @@ class Pokemon
     @type = type
   end
 
-  def self.save(name, type, db)
+  def self.save(name, type, @db)
     @db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
