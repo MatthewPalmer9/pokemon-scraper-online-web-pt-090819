cf75fb4c8df83fa1a2d9198a3829dfc5e512b594
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-14 18:58:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,9 +15,8 @@ class Pokemon
 
   def self.find(num, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [num])
-    binding.pry
-    new_pokemon = self.new(pokemon)
-    #binding.pry
+    new_pokemon = self.new()
+    binding.pry 
     new_pokemon.id = pokemon[0][0]
     new_pokemon
   end
